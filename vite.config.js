750c3be8c58d4fd7cea2ea1ded98c7045ab1e6d8
SrGexj <srgexj@gmail.com>
2025-01-19 21:00:33 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        require: 'undefined', // Evita que el navegador busque 'require' globalmente
-      },
-    },
+    include: ['react-quilljs', 'quill'],
   },
-})
+});
